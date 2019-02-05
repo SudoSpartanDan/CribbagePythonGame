0bd9a7f63ff407b5ee21ee16aea028ab6bfa581c
Daniel Thompson <sudospartandan@gmail.com>
2019-02-05 13:37:52 +0300

    15's logic

--- cribbage.py
+++ cribbage.py
@@ -1,11 +1,11 @@
+import random
 from game import Game
 
-def main():
-    game = Game()
-    print(game.getScoreBoardString(), '\n')
-    print('Dealing Cards', '\n')
-    game.dealCards()
-    print(game.getPlayerHandString(), '\n')
+def pegPhase(game: Game):
+    currentChain = []
+    #game.crib.append(game.cpu.hand.pop(random.randrange(5)))
+
+def discardPhase(game: Game):
     while True:
         try:
             firstCard = int(input('Choose first card to give to crib (1-6): '))
@@ -18,7 +18,7 @@ def main():
         else:
             game.crib.append(game.player.hand.pop(firstCard-1))
             break
-
+    print()
     print(game.getPlayerHandString(), '\n')
 
     while True:
@@ -27,14 +27,33 @@ def main():
         except ValueError:
             print('Error: Please enter a number 1-5.')
             continue
-        if(firstCard < 1 or firstCard > 5):
+        if(secondCard < 1 or secondCard > 5):
             print('Error: Please enter a number 1-5.')
             continue
         else:
-            game.crib.append(game.player.hand.pop(firstCard-1))
+            game.crib.append(game.player.hand.pop(secondCard-1))
             break
 
-    print(game.getPlayerHandString(), '\n')
-    print(game.getCribHandString(), '\n')
+    # CPU Discard
+    # First
+    game.crib.append(game.cpu.hand.pop(random.randrange(6)))
+    # Second
+    game.crib.append(game.cpu.hand.pop(random.randrange(5)))
+
+def main():
+    game = Game()
+    # Start the game
+    while True:
+        print('Dealing Cards', '\n')
+        game.dealCards()
+        print(game.getPlayerHandString(), '\n')
+        discardPhase(game)
+        print()
+        print(game.getPlayerHandString(), '\n')
+        print(game.cutCard, '\n')
+        game.calculateScore()
+        print(game.getScoreBoardString(), '\n')
+        pegPhase(game)
+        break
 
 main()
--- game.py
+++ game.py
@@ -1,6 +1,22 @@
+import math
+import itertools
 from player import Player
 from card import Card, CardSuit, CardValue
 from deck import Deck
+
+def findFifteens(cards, partial=[]):
+    cardsTotal = sum(min(card.value.value, 10) for card in partial)
+
+    if cardsTotal == 15:
+        print('15')
+        print(' '.join(['%s' % c for c in partial]))
+    if cardsTotal >= 15:
+        return
+    
+    for i in range(len(cards)):
+        card = cards[i]
+        remaining = cards[i+1:]
+        findFifteens(remaining, partial + [card])
 
 class Game:
     def __init__(self):
@@ -10,17 +26,24 @@ class Game:
         self.deck = Deck()
         self.crib = []
 
-    def getScoreBoardString(self):
-        playerScore = '{0:16s}: {1:3d}'.format(self.player.name, self.player.score)
-        cpuScore = '{0:16s}: {1:3d}'.format(self.cpu.name, self.cpu.score)
-        return '{0}\n{1}'.format(playerScore, cpuScore)
-
     def dealCards(self):
         self.deck.shuffle()
         for i in range(6):
             self.player.hand.append(self.deck.deal())
             self.cpu.hand.append(self.deck.deal())
         self.cutCard = self.deck.cut()
+
+    def calculateScore(self):
+        # Player
+        playerHand = self.player.hand
+        playerHand.append(self.cutCard)
+        findFifteens(playerHand)
+        
+
+    def getScoreBoardString(self):
+        playerScore = '{0:16s}: {1:3d}'.format(self.player.name, self.player.score)
+        cpuScore = '{0:16s}: {1:3d}'.format(self.cpu.name, self.cpu.score)
+        return '{0}\n{1}'.format(playerScore, cpuScore)
 
     def getPlayerHandString(self):
         return ' '.join(['%s' % c for c in self.player.hand])
